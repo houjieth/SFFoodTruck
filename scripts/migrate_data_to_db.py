7904a6ceff87a95cea4ca70837016ad479fbf29c
Jie Hou <houjieth@gmail.com>
2014-11-07 22:10:02 -0800

    Add script for loading data into mongodb

---
+++ scripts/migrate_data_to_db.py
@@ -0,0 +1,22 @@
+import json
+
+from pymongo import MongoClient
+
+WORK_DB = 'test'
+WORK_COLLECTION = 'SFFoodTruck'
+INPUT_FILE_NAME = 'food_truck_data.json'
+
+client = MongoClient()
+db = client[WORK_DB]
+collection = db[WORK_COLLECTION]
+
+# clean the working collection if we already have one
+if collection.count() > 0:
+    collection.drop()
+
+input_file = open(INPUT_FILE_NAME, "r")
+trucks_json = input_file.read()
+trucks = json.loads(trucks_json)
+
+collection.insert(trucks)
+
